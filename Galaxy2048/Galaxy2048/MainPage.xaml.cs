52f3f7fa01bbd578b96cc784299c5e5e29383567
Allen Davis <corranforce@msn.com>
2019-07-05 21:46:48 -0500

    Add project files.

---
+++ Galaxy2048/Galaxy2048/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Galaxy2048
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Browser.Source = "https://corranforce.com/2048";
+        }
+
+        private void Browser_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            LoadingLabel.IsVisible = true;
+        }
+
+        private void Browser_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            LoadingLabel.IsVisible = false;
+        }
+    }
+}
