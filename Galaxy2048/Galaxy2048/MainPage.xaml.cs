46299931c02ab27abba6b2b91b98479d82531e5d
Allen Davis <corranforce@msn.com>
2019-07-07 19:50:44 -0500

    Added new moon icon

--- Galaxy2048/Galaxy2048/MainPage.xaml.cs
+++ Galaxy2048/Galaxy2048/MainPage.xaml.cs
@@ -5,6 +5,7 @@ namespace Galaxy2048
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
+
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
